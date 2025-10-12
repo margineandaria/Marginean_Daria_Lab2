bbf6afb489e4486e106ae83ead45499268761a40
margineandaria <margineandariam@gmail.com>
2025-10-12 20:17:01 +0300

    Initial commit

---
+++ Marginean_Daria_Lab2/Models/Publisher.cs
@@ -0,0 +1,8 @@
+namespace Marginean_Daria_Lab2.Models;
+
+public class Publisher
+{
+    public int ID { get; set; }
+    public string PublisherName { get; set; }
+    public ICollection<Book>? Books { get; set; }
+}
