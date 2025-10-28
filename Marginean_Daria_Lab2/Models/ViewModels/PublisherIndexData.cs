3cd1990cc1719f0ce3703b9c12ccaa4037eac079
margineandaria <margineandariam@gmail.com>
2025-10-28 21:13:38 +0200

    Modif Details

---
+++ Marginean_Daria_Lab2/Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,8 @@
+using Marginean_Daria_Lab2;
+namespace Marginean_Daria_Lab2.Models.ViewModels;
+
+public class PublisherIndexData
+{
+    public IEnumerable<Publisher> Publishers { get; set; }
+    public IEnumerable<Book> Books { get; set; }
+}
